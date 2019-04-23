b1e4bc47e5b3c408bc40c8658dd7841f780d8902
Arne Siemens <arne.siemens@scalamat.de>
2019-04-23 18:08:28 +0200

    ng-prime via npm; basic routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import HomeModule from './home/home.module';
+import ContactModule from './contact/contact.module';
+import TermsModule from './terms/terms.module';
+import DataProtectionModule from './data-protection/data-protection.module';
+import ImprintModule from './imprint/imprint.module';
+import DeliverModule from './deliver/deliver.module';
+import ReturnModule from './return/return.module';
+import CategoriesModule from './categories/categories.module';
+// import { CategoryComponent } from './category/category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => HomeModule },
+  { path: 'contact', loadChildren: () => ContactModule },
+  { path: 'terms', loadChildren: () => TermsModule },
+  { path: 'data-protection', loadChildren: () => DataProtectionModule },
+  { path: 'imprint', loadChildren: () => ImprintModule },
+  { path: 'deliver', loadChildren: () => DeliverModule },
+  { path: 'return', loadChildren: () => ReturnModule },
+  { path: 'category', loadChildren: () => CategoriesModule },
+  { path: 'categories', loadChildren: () => CategoriesModule },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wizard-shop-standard';
+  navigation = [
+    { url: '', context: 'Home' },
+    { url: 'contact', context: 'Kontakt' },
+    { url: 'terms', context: 'AGB' },
+    { url: 'data-protection', context: 'Datenschutz' },
+    { url: 'imprint', context: 'Impressum' },
+    { url: 'deliver', context: 'Versandbedingungen' },
+    { url: 'return', context: 'Rückgabebedingungen' },
+    { url: 'category/id/1', context: 'Kategorie' },
+    { url: 'categories', context: 'Kategorien' },
+  ];
 }
